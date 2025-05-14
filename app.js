9092d6d3a9180782eb9dec44763bacb52da78a59
Anasmemon670 <memonanas676@gmail.com>
2025-05-14 16:59:36 +0500

    quiz

---
+++ app.js
@@ -0,0 +1,98 @@
+const startBtn = document.querySelector('.Start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const ContinueBtn = document.querySelector('.Continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+
+startBtn.addEventListener("click", () => {
+    popupInfo.classList.add("active");
+    main.classList.add("active")
+
+});
+
+exitBtn.addEventListener("click", () => {
+    popupInfo.classList.remove("active");
+    main.classList.remove("active")
+
+});
+
+ContinueBtn.addEventListener("click", () => {
+    quizSection.classList.add("active");
+    popupInfo.classList.remove("active");
+    main.classList.remove("active");
+    quizBox.classList.add("active");
+
+    ShowQuestion(0);
+    QuestionCounter(1);
+});
+
+let questionCount = 0;
+let QuestionNumber = 1;
+
+let OptionList = document.querySelector(".option-list")
+let nextBtn = document.querySelector(".next-btn");
+
+nextBtn.addEventListener("click", () => {
+    if (questionCount < Questions.length - 1) {
+        questionCount++;
+        ShowQuestion(questionCount);
+
+        QuestionNumber++;
+        QuestionCounter(QuestionNumber);
+    } else {
+        console.log('Questions Completed');
+    }
+});
+
+function ShowQuestion(index) {
+    let QuestionsText = document.querySelector(".question-text");
+    QuestionsText.textContent = `${Questions[index].numb}. ${Questions[index].question}`;
+
+    let OptionTag = `<div class="option"><span>${Questions[index].Option[0]}</span></div>
+                     <div class="option"><span>${Questions[index].Option[1]}</span></div>
+                     <div class="option"><span>${Questions[index].Option[2]}</span></div>
+                     <div class="option"><span>${Questions[index].Option[3]}</span></div>`;
+
+    OptionList.innerHTML = OptionTag;
+
+    let Option = document.querySelectorAll(".option");
+    for (let i = 0; i < Option.length; i++) {
+        Option[i].setAttribute('onclick', 'OptionSelected(this)');
+    };
+
+};
+
+function QuestionCounter(index) {
+    let TotalQuestion = document.querySelector(".question-total");
+
+    TotalQuestion.textContent = `${index} of ${Questions.length} Questions`
+};
+
+function OptionSelected(answer) {
+    let UserAnswer = answer.innerText;
+    let CorrectAnswer = Questions[questionCount].answer;
+    let AllOptions = OptionList.children.length;
+
+    if (UserAnswer === CorrectAnswer) {
+        answer.classList.add("Correct");
+    } else {
+        answer.classList.add("InCorrect");
+
+        // ✅ Correct answer auto highlight
+        for (let i = 0; i < AllOptions; i++) {
+            let optionText = OptionList.children[i].querySelector("span").innerText;
+
+            if (optionText === CorrectAnswer) {
+                OptionList.children[i].classList.add("Correct");
+            }
+        }
+    }
+
+    // ❌ Disable all options after selection
+    for (let i = 0; i < AllOptions; i++) {
+        OptionList.children[i].classList.add("disabled");
+    }
+}
+
---
+++ question.js
@@ -0,0 +1,58 @@
+let Questions = [
+    {
+        numb: 1,
+        question: "What does the setInterval() function do in JavaScript?",
+        answer: "B.Repeats a function at specified intervals.",
+        Option: [
+            "A.Executes a function only once.",
+            "B.Repeats a function at specified intervals.",
+            "C.Stops a function from executing.",
+            "D.Loads a new web page."
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the purpose of <input type='checkbox'> in HTML?",
+        answer: "B.To allow multiple options to be selected.",
+        Option: [
+            "A.To create a dropdown menu",
+            "B.To allow multiple options to be selected.",
+            "C.To enter single-line text.",
+            "D.To display an image."
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which CSS property is used to change the background color of an element?",
+        answer: "C.background-color.",
+        Option: [
+            "A.Color.",
+            "B.Bg Color.",
+            "C.background-color.",
+            "D.Background."
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the purpose of localStorage in JavaScript?",
+        answer: "B.To permanently store data in the browser.",
+        Option: [
+            "A.To fetch data from a server.",
+            "B.To permanently store data in the browser.",
+            "C.To temporarily store user data.",
+            "D.To reload the web page.."
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Does HTML Stand For?",
+        answer: "A.Hyper Text Markup Language.",
+        Option: [
+            "A.Hyper Text Markup Language.",
+            "B.Hyper Type Multiple Language.",
+            "C.Hyper Type Multi Language.",
+            "D.Hyper Text Multiple Language."
+        ]
+    },
+]
